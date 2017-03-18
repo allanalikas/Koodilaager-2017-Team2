3313a07c352d36cbb3b0cecc767e851c5fa1aea3
Jako Aimsalu <Jako@Jako.local>
2017-03-18 22:11:21 +0200

    area

--- Hero.java
+++ Hero.java
@@ -13,6 +13,9 @@ public class Hero{
     float pos_x = 70;
     float pos_y = 100;
 
+    int box_height = 40;
+    int box_width = 40;
+
     float speed = 1.5f;
 
     float mouse_X = pos_x;
@@ -23,26 +26,46 @@ public class Hero{
     public void update(GameContainer gc, int delta, int camera_x, int camera_y, int map_height, int map_width, int tilesize) {
         Input inp = gc.getInput();
 
+        int height = map_height * tilesize;
+        int width = map_width * tilesize;
+
         if (inp.isMousePressed(0b1)) {
-            mouse_X = inp.getMouseX() + camera_x;
-            mouse_Y = inp.getMouseY()+ camera_y;
+            if (((inp.getMouseX() + camera_x) > (width - box_width))){
+                mouse_X = width - box_width;
+            }
+            else if (((inp.getMouseX() + camera_x) < 0)) {
+                mouse_X = 0;
+            }
+            else {
+                mouse_X = inp.getMouseX() + camera_x;
+            }
+
+            if (((inp.getMouseY() + camera_y) > (height - box_height))){
+                mouse_Y = height - box_height;
+            }
+            else if (((inp.getMouseY() + camera_y) < 0)) {
+                mouse_Y = 0;
+            }
+            else {
+                mouse_Y = inp.getMouseY() + camera_y;
+            }
         }
 
-        if (mouse_X != pos_x) {
+        if (Math.round(mouse_X) != Math.round(pos_x)) {
             float dx = (mouse_X - pos_x) / ((float) Math.sqrt((float) Math.pow(mouse_X - pos_x, 2) + (float) Math.pow(mouse_Y - pos_y, 2)) / speed);
             pos_x = pos_x + dx;
         }
-        if (mouse_X != pos_x) {
+        if (Math.round(mouse_Y) != Math.round(pos_y)) {
             float dy = (mouse_Y - pos_y) / ((float) Math.sqrt((float) Math.pow(mouse_X - pos_x, 2) + (float) Math.pow(mouse_Y - pos_y, 2)) / speed);
             pos_y = pos_y + dy;
         }
 
 
         }
-    public void render (GameContainer gc, Graphics g, int camera_x, int camera_y){
+    public void render(GameContainer gc, Graphics g, int camera_x, int camera_y){
 
         g.setColor(new Color(255, 255, 30));
-        g.fillRect(pos_x - camera_x , pos_y - camera_y, 40, 45);
+        g.fillRect(pos_x - camera_x , pos_y - camera_y, box_width, box_height);
     }
 
 }
