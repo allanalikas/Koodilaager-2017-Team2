9227d20717f91c5de943f05622a30b329f16057d
Hendrik Eerikson <hendrik.eerikson@gmail.com>
2017-03-18 13:26:45 +0200

    INITAL COMMIT

---
+++ InGame.java
@@ -0,0 +1,26 @@
+import org.newdawn.slick.state.*;
+import org.newdawn.slick.*;
+
+
+public class InGame extends BasicGameState {
+
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
+
+    }
+
+    public void update(GameContainer gc, StateBasedGame sbg, int Delta) throws SlickException{
+
+    }
+
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
+
+    }
+
+    public void mousePressed(int button, int mx, int my) {
+
+    }
+
+    public int getID(){
+        return 0;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,35 @@
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.StateBasedGame;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main extends StateBasedGame {
+
+    public static int screenW = 1000;
+    public static int screenH = 600;
+
+    public Main(String AppName){
+        super(AppName);
+    }
+
+    public void initStatesList(GameContainer gc) throws SlickException {
+        this.addState(new InGame());
+    }
+
+    public static void main(String[] args) {
+
+        try
+        {
+            AppGameContainer appgc;
+            appgc = new AppGameContainer(new Main("TEAM1"));
+            appgc.setDisplayMode(screenW, screenH, false);
+            appgc.start();
+        }
+        catch (SlickException ex)
+        {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
